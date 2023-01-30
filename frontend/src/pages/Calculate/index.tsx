9276285c01172ea077374634d05608d5aa6cbfc1
Никита Яхин <mr.y.nikita@gmail.com>
2023-01-30 19:27:10 +0500

    create from calculate, append li calculate

--- frontend/src/pages/Calculate/index.tsx
+++ frontend/src/pages/Calculate/index.tsx
@@ -1,10 +1,41 @@
+import * as SC from './style';
 import LayoutMain from "../../UI/Layouts/Main";
-
+import { useState } from 'react';
 
 const PageCalculate = () => {
 
+    const [negative, setNegative] = useState(false);
+    const [fractional, setFractional] = useState(false);
+
     return (
-        <LayoutMain chapter="Calculate"/>
+        <LayoutMain chapter="Calculate">
+            <SC.Form>
+
+                <SC.WrapperNumber>
+                    <input placeholder='Целая часть' />
+                    <input placeholder='Дробная часть' disabled={!fractional} />
+                </SC.WrapperNumber>
+
+                <fieldset>
+
+                    <legend>Выберите свойства числа:</legend>
+                    <div>
+                        <div>
+                            <input type='checkbox' onChange={_ => setNegative(!negative)}/>
+                            <label>Отрицательное</label>
+                        </div>
+                        <div>
+                            <input type='checkbox' onChange={_ => setFractional(!fractional)}/>
+                            <label>Дробное</label>
+                        </div>
+                    </div>
+                </fieldset>
+
+                <button>Посчитать</button>
+
+            </SC.Form>
+            <SC.List></SC.List>
+        </LayoutMain>
     );
 
 };
--- frontend/src/pages/Calculate/style.ts
+++ frontend/src/pages/Calculate/style.ts
@@ -1,1 +1,13 @@
 import styled from "styled-components";
+
+export const WrapperNumber = styled.div`
+display: flex;
+& > * {
+    margin-left: 10px;
+}
+`;
+export const List = styled.li`
+`;
+export const Form = styled.form`
+position: relative;
+`;
