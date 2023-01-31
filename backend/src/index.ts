dc0b2730549b38de2f788bde369fa58496f399df
Никита Яхин <mr.y.nikita@gmail.com>
2023-01-31 13:17:54 +0500

    append get message endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,31 @@
+import { readFileSync, writeFileSync, } from 'fs';
 import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+
+app.get('/calculate', (req, res) => {
+
+
+
+});
+app.get('/message', (req, res) => {
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.json(JSON.parse(readFileSync('./json/message.json', 'utf8')));
+
+});
+app.post('/calculate', (req, res) => {
+
+
+
+});
+app.post('/message', (req, res) => {
+
+
+
+});
+
+app.listen(8000);
--- frontend/src/pages/Message/index.tsx
+++ frontend/src/pages/Message/index.tsx
@@ -1,14 +1,34 @@
 import * as SC from './style';
 import LayoutMain from "../../UI/Layouts/Main";
 import Message from '../../components/Message';
+import { useEffect, useState } from 'react';
 
 
 const PageMessage = ({}) => {
 
+    const [messages, setMessages] = useState<{ text: string, author?: string }[]>([]);
+
+    useEffect(() => {
+
+        const res = fetch('http://127.0.0.1:8000/message', {
+
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+
+        }).then(res => res.json()).then(result => {
+
+            setMessages(result);
+
+        });
+
+    }, []);
+
     return (
         <LayoutMain chapter="Message">
             <SC.Chat>
-                <Message text='Hello.'/>
+                {messages.map((m, mi) => <Message key={mi} {...m}></Message>)}
             </SC.Chat>
             <SC.Form>
                 <SC.Input placeholder='Введите новое сообщение'></SC.Input>
